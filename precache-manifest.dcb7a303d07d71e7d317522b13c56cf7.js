41c1bb8bccde98121e95c018e0c2d8e3c477dcd2
Shahriar Kabir <bdshahriar@gmail.com>
2019-04-13 23:49:31 +0600

    Updates

---
+++ precache-manifest.dcb7a303d07d71e7d317522b13c56cf7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactapp/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "699d48caefa08891bb24",
+    "url": "/reactapp/static/js/runtime~main.7f5faac6.js"
+  },
+  {
+    "revision": "1fc25bf965eb172aea57",
+    "url": "/reactapp/static/js/main.48f0fd9e.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/reactapp/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "1fc25bf965eb172aea57",
+    "url": "/reactapp/static/css/main.9fe1c6f2.chunk.css"
+  },
+  {
+    "revision": "7d64f2e7c80811d6cca04cc465187f40",
+    "url": "/reactapp/index.html"
+  }
+];
